9057752bc90e318c98e7adc0407c7a1b3d718721
Rumata Estorsky <rumata@sputnikchess.ru>
2015-04-10 21:49:36 +0300

    #2 Описать сущности в БД + модель User + щепотка авторизации

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,68 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import models.User;
+import play.data.Form;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
 
+import static play.data.Form.form;
 import views.html.*;
 
 public class Application extends Controller {
+    // -- Authentication
+
+    public static class Login {
+
+        public String email;
+        public String password;
+
+        public String validate() {
+            if (User.authenticate(email, password)) { //TODO  == null
+                return "Invalid user or password";
+            }
+            return null;
+        }
+
+    }
 
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
+
+
+    /**
+     * Login page.
+     */
+    public static Result login() {
+        return ok(
+            login.render(form(Login.class))
+        );
+    }
+
+    /**
+     * Handle login form submission.
+     */
+    public static Result authenticate() {
+        Form<Login> loginForm = form(Login.class).bindFromRequest();
+        if (loginForm.hasErrors()) {
+            return badRequest(login.render(loginForm));
+        } else {
+            session("email", loginForm.get().email);
+            return redirect(""); ///TODO
+        }
+    }
+
+    /**
+     * Logout and clean the session.
+     */
+    public static Result logout() {
+        session().clear();
+        flash("success", "You've been logged out");
+        return redirect(
+                routes.Application.login()
+        );
+    }
+
 
 }
---
+++ app/models/User.java
@@ -0,0 +1,70 @@
+package models;
+
+import com.avaje.ebean.annotation.Encrypted;
+import org.hibernate.validator.constraints.Length;
+import play.data.validation.Constraints;
+import play.db.ebean.Model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Users
+ */
+@Entity
+@Table(name = "users", schema="public")
+//@Label("Users")
+public class User extends Model {
+    /**
+     * Id
+     */
+    @Id
+//    @Label("Id")
+    public Integer id;
+    /**
+     * Email
+     */
+    @Column(unique = true)
+    @Length(min = 0, max = 255)
+    @Constraints.Email
+//    @Label("Email")
+    public String email;
+    /**
+     * Password
+     */
+    @Length(min = 0, max = 32)
+//    @Encrypted(dbEncryption = false)
+//    @Label("Password")
+    public String password;
+
+    /**
+     * Role
+     */
+    @Length(min = 0, max = 16)
+//    @Label("Role")
+    public String role;
+
+//    public static final Model.Finder<Integer, User> find = new Model.Finder<>(Integer.class, User.class);
+
+    @Override
+    public String toString() {
+        return " <" + email + ">";
+    }
+
+
+    /**
+     * Authenticate a User.
+     */
+    //TODO return User
+    public static boolean authenticate(String email, String password) {
+        return email.equals("valera.dt@gmail.ru")  && password.equals("123456");
+//        return find.where()
+//                .eq("email", email)
+//                .eq("password", password)
+//                .findUnique();
+    }
+
+
+}
