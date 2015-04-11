01ce641f72015af8ce1b9bef20f2c27f7679b618
Andrej.Belyakov <Andrej.Belyakov@tstn.ru>
2015-04-11 23:07:46 +0300

    Problem with abstract entities fields generation fiexed;

--- app/models/IDNameEntity.java
+++ app/models/IDNameEntity.java
@@ -3,10 +3,16 @@ package models;
 import utils.StringUtils;
 
 import javax.persistence.Column;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.MappedSuperclass;
 
 /**
  * Created by andy on 4/11/15.
  */
+
+@MappedSuperclass
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class IDNameEntity extends AbstractBaseEntity{
     @Column(unique = true)
     protected String name;
--- app/models/IDNameTimeEntity.java
+++ app/models/IDNameTimeEntity.java
@@ -1,16 +1,22 @@
 package models;
 
 import javax.persistence.Column;
-import java.sql.Date;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.MappedSuperclass;
+import java.util.Date;
+
 
 /**
  * Created by rumata on 4/11/15.
  */
-public class IDNameTimeEntity extends IDNameEntity {
-    @Column()
+@MappedSuperclass
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public abstract class IDNameTimeEntity extends IDNameEntity {
+    @Column(name = "created_at")
     protected Date createdAt;
 
-    @Column()
+    @Column(name = "modified_at")
     protected Date modifiedAt;
 
     public Date getCreatedAt() {
