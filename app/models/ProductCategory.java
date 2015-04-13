e48fe3ed9aac9e7dc576d984d5d0dfedf29dd8dc
Andrej.Belyakov <Andrej.Belyakov@tstn.ru>
2015-04-13 20:55:36 +0300

    Hierarchical categories

--- app/models/ProductCategory.java
+++ app/models/ProductCategory.java
@@ -1,8 +1,9 @@
 package models;
 
-import javax.persistence.Entity;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by andy on 4/11/15.
@@ -12,6 +13,48 @@ import javax.persistence.Table;
 @SequenceGenerator(name = "entity_id_gen", sequenceName = "productcategories_id_seq")
 public class ProductCategory extends IDNameTimeEntity {
 
+    @ManyToOne
+    private ProductCategory parent;
+
+    @NotNull
+    @OneToMany(mappedBy = "parent")
+    private Set<ProductCategory> subCategories = new HashSet<>();
+
+    public ProductCategory() {
+    }
+
+    public ProductCategory(final ProductCategory parent) {
+        if(parent == null) throw new IllegalArgumentException("Parent required!");
+        this.parent = parent;
+        registerInParentsChilds();
+    }
+
+    private void registerInParentsChilds() {
+        this.parent.subCategories.add(this);
+    }
+
+    public void move(final ProductCategory newParent)  {
+        if(parent == null) throw new IllegalArgumentException("Parent required!");
 
 
+        this.parent.subCategories.remove(this);
+        this.parent = newParent;
+        registerInParentsChilds();
+    }
+
+    public ProductCategory getParent() {
+        return parent;
+    }
+
+    public void setParent(ProductCategory parent) {
+        this.parent = parent;
+    }
+
+    public Set<ProductCategory> getSubCategories() {
+        return subCategories;
+    }
+
+    public void setSubCategories(Set<ProductCategory> subCategories) {
+        this.subCategories = subCategories;
+    }
 }
