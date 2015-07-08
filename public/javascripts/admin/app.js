9cd7f12e66d6c61fd9d786977fe3c688b9df83e2
Pavel Shvedov <shvedov.pavel.a@gmail.com>
2015-07-08 20:32:50 +0200

    Angular admin page bootstrap

---
+++ public/javascripts/admin/app.js
@@ -0,0 +1,35 @@
+(function() {
+
+	var app = angular.module('adminPanelApp', ['ngRoute']);
+
+	app.config(function($routeProvider) {
+
+		$routeProvider
+			.when("/", {
+				controller: "AdminIndexController",
+				templateUrl: "/assets/templates/admin/index.html"
+			})
+			.when("/pages", {
+				controller: "PagesListController",
+				templateUrl: "/assets/templates/admin/pages_list.html"
+			})
+			.when("/pages/edit/:pageId", {
+				controller: "PagesEditController",
+				templateUrl: "/assets/templates/admin/pages_edit.html"
+			}).
+			when("/pages/create/", {
+				controller: "PagesCreateController",
+				templateUrl: "/assets/templates/admin/pages_create.html"
+			}).
+			when("/categories", {
+				redirectTo: "/"
+			}).
+			when("/media", {
+				redirectTo: "/"
+			}).
+			otherwise({
+				redirectTo: "/"
+			});
+	});
+
+})();
---
+++ public/javascripts/admin/controllers/coreControllers.js
@@ -0,0 +1,9 @@
+(function() {
+
+	var app = angular.module('adminPanelApp');
+
+	app.controller('AdminIndexController', ["$scope", function($scope) {
+		
+	}]);
+
+})();
